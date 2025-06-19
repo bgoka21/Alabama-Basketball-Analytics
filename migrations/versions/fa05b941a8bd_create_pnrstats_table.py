6b9fb4a7304f20cf9dc29fb229634fd2c74252d6
Brady Goka <bgoka21@gmail.com>
2025-06-19 12:40:09 -0500

    Hand-author migration to create pnr_stats table

---
+++ migrations/versions/fa05b941a8bd_create_pnrstats_table.py
@@ -0,0 +1,24 @@
+"""Create PnRStats table
+
+Revision ID: fa05b941a8bd
+Revises: 929486bc4df5
+Create Date: 2025-06-19 12:39:06.122471
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fa05b941a8bd'
+down_revision = '929486bc4df5'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
