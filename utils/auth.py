d10216959ea1d2356824943b50c61826ffa3fea4
bgoka21 <bgoka21@gmail.com>
2025-07-25 12:43:34 -0500

    Allow players to view skill dev page

--- utils/auth.py
+++ utils/auth.py
@@ -4,6 +4,9 @@ from flask_login import current_user, login_required
 
 # Endpoints a logged-in player is allowed to access without being redirected.
 PLAYER_ALLOWED_ENDPOINTS = {
+    'public.practice_homepage',
+    'public.homepage',
+    'public.skill_dev',
     'admin.player_detail',
     'admin.logout',
     'admin.edit_skill_entry',
