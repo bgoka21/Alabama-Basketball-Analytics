f7d9941b59a4d73a38a8c097b427cb04b15510b2
Brady Goka <bgoka21@gmail.com>
2025-10-03 22:43:51 +0000

    Revert "Merge pull request #688 from bgoka21/codex/add-leaderboard-snapshot-model-and-migration"
    This reverts commit cc3f7ad1acbed73def745c9599f2bbf98857c51d, reversing
    changes made to b799eb0158c42da898e8452acbde7de0377d7a37.

--- app/cli/__init__.py
+++ app/cli/__init__.py
@@ -2,4 +2,3 @@
 
 from .import_draft_stock import import_draft_stock  # noqa: F401
 from .backfill_coach_names import backfill_coach_names  # noqa: F401
-from .refresh_leaderboard_snapshots import refresh_leaderboard_snapshots  # noqa: F401
