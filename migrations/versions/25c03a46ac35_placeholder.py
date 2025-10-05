e044adf073c33291da58bc4bf485a617e3cfa55c
Brady Goka <bgoka21@gmail.com>
2025-10-05 19:34:56 +0000

    Fix Alembic chain: add placeholder parent for cached_leaderboards

---
+++ migrations/versions/25c03a46ac35_placeholder.py
@@ -0,0 +1,15 @@
+"""Placeholder to satisfy down_revision chain."""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '25c03a46ac35'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
