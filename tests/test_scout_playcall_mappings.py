9c4568384a6d05f3712c3882f319778d2b928089
bgoka21 <bgoka21@gmail.com>
2025-12-13 11:50:14 -0600

    Remove playcall series customization route

--- tests/test_scout_playcall_mappings.py
+++ tests/test_scout_playcall_mappings.py
@@ -1,14 +1,7 @@
 from pathlib import Path
 
-from models.scout import (
-    ScoutGame,
-    ScoutPlaycallMapping,
-    ScoutPossession,
-    ScoutTeam,
-    normalize_playcall,
-)
+from models.scout import ScoutGame, ScoutPlaycallMapping, ScoutPossession, ScoutTeam
 from scout.parsers.scout_playcalls import store_scout_playcalls
-from scout.routes import _save_playcall_mapping
 from models.database import db
 
 
@@ -43,58 +36,3 @@ def test_store_scout_playcalls_applies_saved_mapping(tmp_path, app):
         assert possession.series == 'Mapped Series'
         assert possession.family == 'Mapped Family'
         assert possession.playcall == 'Set A'
-
-
-def test_save_playcall_mapping_backfills_all_possessions(app):
-    with app.app_context():
-        team = ScoutTeam(name='Backfill Team')
-        db.session.add(team)
-        db.session.flush()
-
-        game_one = ScoutGame(scout_team_id=team.id)
-        game_two = ScoutGame(scout_team_id=team.id)
-        db.session.add_all([game_one, game_two])
-        db.session.flush()
-
-        possession_one = ScoutPossession(
-            scout_game_id=game_one.id,
-            instance_number='1',
-            playcall='Play X',
-            series='Old',
-            family='Legacy',
-            bucket='STANDARD',
-            points=0,
-        )
-        possession_two = ScoutPossession(
-            scout_game_id=game_two.id,
-            instance_number='2',
-            playcall='play x',
-            series='Old',
-            family='Legacy',
-            bucket='STANDARD',
-            points=0,
-        )
-        db.session.add_all([possession_one, possession_two])
-        db.session.commit()
-
-        updated = _save_playcall_mapping(
-            'Play X',
-            'New Series',
-            'New Family',
-            {game_one.id},
-            apply_globally=True,
-        )
-        db.session.commit()
-
-        assert updated == 2
-
-        mapping = ScoutPlaycallMapping.query.filter_by(
-            playcall_key=normalize_playcall('Play X')
-        ).first()
-        assert mapping is not None
-        assert mapping.canonical_series == 'New Series'
-        assert mapping.canonical_family == 'New Family'
-
-        refreshed = ScoutPossession.query.order_by(ScoutPossession.instance_number).all()
-        assert {pos.series for pos in refreshed} == {'New Series'}
-        assert {pos.family for pos in refreshed} == {'New Family'}
