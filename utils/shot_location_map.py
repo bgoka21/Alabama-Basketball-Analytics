58253df9775ef4f560d03665723c7e192df5f03a
bgoka21 <bgoka21@gmail.com>
2026-01-15 16:35:07 -0600

    Add shot location mappings for left SW and nail

--- utils/shot_location_map.py
+++ utils/shot_location_map.py
@@ -21,6 +21,8 @@ SHOT_LOCATION_MAP: Dict[str, str] = {
     "Right Houston": "short_wing_right",
     "Left SC": "short_corner_left",
     "Right SC": "short_corner_right",
+    "Left SW": "short_wing_left",
+    "Nail": "paint",
     "Top": "top",
     "Logo": "logo",
     "Right SW": "short_wing_right",
